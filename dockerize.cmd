c18a51646eff42c6d2c49050fb32f55078aea457
Pascal Hubacher <pascal.hubacher@outlook.com>
2023-03-19 16:29:40 +0100

    docker image ready

--- dockerize.cmd
+++ dockerize.cmd
@@ -1,4 +1,6 @@
-docker build -t general-worker-ball-zone-change:0.0.1 -t general-worker-ball-zone-change:latest .
+REM docker build -t general-worker-ball-zone-change:0.0.1 -t general-worker-ball-zone-change:latest .
+docker rmi general-worker-ball-zone-change:latest -f
+docker build -t general-worker-ball-zone-change:latest .
 
 REM if it runs on localhost
 REM docker run -it -p 8080:8080 general-worker-ball-zone-change:latest
